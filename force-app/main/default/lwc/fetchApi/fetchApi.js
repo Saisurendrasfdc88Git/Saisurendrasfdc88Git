2977cbfca74dc376a8e1e5ee8cde80f2af7337c3
Sai Surendra Seshapu <133946327+Saisurendrasfdc88Git@users.noreply.github.com>
2023-09-30 12:27:49 +0530

    upload

---
+++ force-app/main/default/lwc/fetchApi/fetchApi.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+
+export default class FetchApi extends LightningElement {
+
+
+    imgURL;
+    endpoint ="https://some-random-api.ml/animal/dog/";
+
+    getImageHandler(){
+        fetch(this.endpoint,{method:'GET'})
+        .then(resp=>resp.json())
+        .then(resp=>{
+            this.imgURL = resp.image;
+        })
+    }
+}
