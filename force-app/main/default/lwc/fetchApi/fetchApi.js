080df64febf8f9438f8ef5308a66b49612333c86
Sai Surendra Seshapu <133946327+Saisurendrasfdc88Git@users.noreply.github.com>
2023-09-30 16:02:27 +0530

    update

--- force-app/main/default/lwc/fetchApi/fetchApi.js
+++ force-app/main/default/lwc/fetchApi/fetchApi.js
@@ -1,16 +1,17 @@
 import { LightningElement } from 'lwc';
 
 export default class FetchApi extends LightningElement {
-
-
     imgURL;
-    endpoint ="https://some-random-api.ml/animal/dog/";
+    endpoint = 'https://some-random-api.ml/animal/dog';
 
-    getImageHandler(){
-        fetch(this.endpoint,{method:'GET'})
-        .then(resp=>resp.json())
-        .then(resp=>{
-            this.imgURL = resp.image;
-        })
+    getImageHandler() {
+        fetch(this.endpoint, { method: 'GET' })
+            .then(resp => resp.json())
+            .then(resp => {
+                this.imgURL = resp.image;
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     }
 }
